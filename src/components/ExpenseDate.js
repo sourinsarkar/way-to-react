ec403289407701e9be79194bbeeb5dd7bf65d504
Sourin <iamsourinsarkar@gmail.com>
2023-04-12 23:27:19 +0530

    Minor changes

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -11,6 +11,7 @@ function ExpenseDate(props) {
         <div className='expense-date__month'>{month}</div>
         <div className='expense-date__year'>{year}</div>
         <div className='expense-date__day'>{day}</div>
+        
     </div>
   )
 }
