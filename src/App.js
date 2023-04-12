e4bd71a79d0d78eacb1ad4166cbc3f2bfb25fda7
Sourin <iamsourinsarkar@gmail.com>
2023-04-12 23:24:00 +0530

    Implemented child levels ans learned data transfer using props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Car Insurance',
+      amount: 25000,
+      date: new Date(2023, 0, 1)
+    },
+    {
+      id: 'e2',
+      title: 'Grocery',
+      amount: 30000,
+      date: new Date(2023, 1, 2)
+    },
+    {
+      id: 'e3',
+      title: 'Healthcare',
+      amount: 15000,
+      date: new Date(2023, 2, 3)
+    },
+    {
+      id: 'e4',
+      title: 'Education',
+      amount: 50000,
+      date: new Date(2023, 11, 4)
+    },
+  ];
+
   return (
     <>
-    <ExpenseItem />
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
+      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
     </>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
-export default function ExpenseItem() {
+export default function ExpenseItem(props) {
   return (
     <div className="expense-item" >
-        <div>Jan 1, 2023</div>
-        <div className="expense-item__description" >
-            <h2>Car Insurance</h2>
-            <div className="expense-item__price">25000</div>
+      <ExpenseDate date={props.date} ></ExpenseDate>
+        <div className="expense-item__description">
+            <h2>{props.title}</h2>
+            <div className="expense-item__price">{props.amount}</div>
         </div>
     </div>
   )
