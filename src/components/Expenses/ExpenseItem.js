5c796b6aee89ea6aee89f943d0440f1fcf7a55b1
Sourin <iamsourinsarkar@gmail.com>
2023-04-13 23:22:42 +0530

    Tried changing the text content, but didn't go through!

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,15 +4,17 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 export default function ExpenseItem(props) {
+  let title = props.title;
   const clickHandler = () => {
-    console.log('Clicked it');
+    title = 'Something something';
+    console.log("Just clicked");
   }
 
   return (
     <Card className="expense-item" >
       <ExpenseDate date={props.date} ></ExpenseDate>
         <div className="expense-item__description">
-            <h2>{props.title}</h2>
+            <h2>{title}</h2>
             <div className="expense-item__price">{props.amount}</div>
         </div>
         <button onClick={clickHandler}>Change Title</button>
